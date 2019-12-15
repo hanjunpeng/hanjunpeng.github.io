41e82b661372b541b835341bc8fffa6cd2619ff4
hjp <hanjunpeng@yeah.net>
2019-12-15 21:32:59 +0800

    commit

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: '博客 接口文档',
+  description: '博客',
+  base: '/',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/' },
+      { text: 'External', link: '/' }
+    ]
+  }
+}
