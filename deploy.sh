3b67b360881420c85e1d6955987cc8c1a945a27c
hjp <hanjunpeng@yeah.net>
2019-12-15 19:24:09 +0800

    commi

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/hanjunpeng/hanjunpeng.github.io.git master:test
+git push -f https://github.com/hanjunpeng/hanjunpeng.github.io.git master
 
 cd -
